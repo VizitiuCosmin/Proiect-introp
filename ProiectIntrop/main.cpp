45389a820df4bcef4b2f5cd7629cda8cebb49e45
VizitiuCosmin <v.cosmin2496@gmail.com>
2017-01-05 12:47:00 +0200

    Update main.cpp

--- ProiectIntrop/main.cpp
+++ ProiectIntrop/main.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
-
 using namespace std;
-
+#include<iostream>
+#include<fstream>
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x;
+    do
+    {
+        cout<<"1. Joc nou"<<endl;
+        cout<<"2. Lista scoruri"<<endl;
+        cout<<"3. Iesire joc"<<endl;
+        cin>>x;
+    switch(x)
+    {
+    case 1:
+        {
+
+        break;
+        }
+    case 2:
+        {
+
+            break;
+        }
+    case 3:
+        {
+            cout<<"Ai iesit din joc!";
+            break;
+        }
+    }
+    }
+    while(x<3 && x>=1);
     return 0;
 }
